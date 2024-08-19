60ad7d3d698b9fe7853326593e776ca8a964d8f9
Max Petretta <m@maxp.co>
2024-08-19 18:23:48 -0400

    fix: publishSchema

--- ponder.config.ts
+++ ponder.config.ts
@@ -23,7 +23,7 @@ export default createConfig({
     kind: "postgres",
     schema: "indexer",
     // NOTE: publicSchema uses materialized views, which are not supported by PowerSync
-    publishSchema: undefined,
+    publishSchema: "",
   },
   networks: {
     base: {
